1ba7af0c1a83b2b58273e9ac314ab762b591f33a
root <root@raspberrypi.(none)>
2013-12-05 23:20:43 +0000

    modified config.txt to go in /boot/ directory

--- startup.sh
+++ startup.sh
@@ -1,1 +1,8 @@
+# add scripts to bottom of this document:
+# sudo vim ~/.bashrc
+
+# edit config.txt in the /boot/ directory
+
+# makes font BIG!!!
 setfont /usr/share/consolefonts/Lat15-Terminus32x16.psf.gz
+
